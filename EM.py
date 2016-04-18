3296c1af47ed3ff1eff911c17f9d2d0a3f82cba6
meirianto <meiri.anto@gmail.com>
2016-04-17 23:28:39 -0400

    estimate number bidders with average of multiple trials for each dataset instead of estimated moving average

--- EM.py
+++ EM.py
@@ -2,39 +2,56 @@ from Auction_Simulator import Auction_Simulator
 from numpy import average, std
 class EM(object):
 
-	def __init__(self, observed_bids, discount):
+	def __init__(self, observed_bids, discount, num_trials, max_iterations):
 		self.observed_bids = observed_bids
-		self.f_mean = average(observed_bids)
-		self.f_std = std(observed_bids)
-		self.g_lam = None 
 		self.discount = discount
+		self.num_trials = num_trials
+		self.max_iterations = max_iterations
 
 	def learn(self):
 		ok_err = 0.1
-		converged = False
-		self.g_lam = len(self.observed_bids)
-		iterations = 0
-		print "START: ", iterations, self.f_mean, self.f_std, self.g_lam
+		f_mean_list = []
+		f_std_list = []
+		g_lam_list = []
+		iterations = []
+		hit_max = 0
+		for i in range(self.num_trials):
+			print "TRIAL : ", i
+			f_mean = average(self.observed_bids)
+			f_std = std(self.observed_bids)
+			g_lam = len(self.observed_bids)-2
+			iteration = 0			
+			# print "START: ", iteration, f_mean, f_std, g_lam
+			converged = False
 
-		while not converged:
-			iterations += 1
-			auction = Auction_Simulator(self.f_mean, self.f_std, self.g_lam)
-			hidden_bids = auction.simulate_auction(self.observed_bids)
-			highest_bid = [auction.simulate_highest(self.observed_bids)]
+			while not converged and iteration <= self.max_iterations:
+				iteration += 1
+				auction = Auction_Simulator(f_mean, f_std, g_lam)
+				hidden_bids = auction.simulate_auction(self.observed_bids)
+				highest_bid = [auction.simulate_highest(self.observed_bids)]
 
-			all_bids = self.observed_bids + hidden_bids + highest_bid
-			old_f_mean, old_f_std, old_g_lam = self.f_mean, self.f_std, self.g_lam
-			self.f_mean, self.f_std = self.f_max(all_bids)
+				all_bids = self.observed_bids + hidden_bids + highest_bid
+				old_f_mean, old_f_std, old_g_lam = f_mean, f_std, g_lam
+				f_mean, f_std = self.f_max(all_bids)
 
-			#TODO: use estimated moving average to update g_lam
-			self.g_lam = self.discount*len(all_bids) + (1-self.discount)*self.g_lam
-			print "ITERATION: ", iterations, self.f_mean, self.f_std, self.g_lam
+				#TODO: use estimated moving average to update g_lam
+				# g_lam = self.discount*(len(all_bids)-2) + (1-self.discount)*g_lam
+				# print "ITERATION: ", iteration, f_mean, f_std, g_lam
+				g_lam = len(all_bids) - 2
 
-			if (abs(old_f_mean-self.f_mean) <= ok_err and abs(old_f_std-self.f_std) <= ok_err and abs(old_g_lam-self.g_lam) <= 20*ok_err):
-				converged = True
-				# print self.f_mean, self.f_std, self.g_lam, iterations
-		
-		return self.f_mean, self.f_std, self.g_lam, iterations
+				if (abs(old_f_mean-f_mean) <= ok_err and abs(old_f_std-f_std) <= ok_err and abs(old_g_lam-g_lam) <= ok_err):
+					converged = True
+			if iteration > self.max_iterations:
+				hit_max += 1
+			else:
+				f_mean_list.append(f_mean)
+				f_std_list.append(f_std)
+				g_lam_list.append(g_lam)
+				iterations.append(iteration)
+				print "EM RESULT: ", f_mean, f_std, g_lam, iteration
+	
+		print " too many iterations %d times", hit_max
+		return average(f_mean_list), average(f_std_list), average(g_lam_list)
 
 	def f_max(self, bids):
 		return average(bids), std(bids)
--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def main():
 	# Simulated Dataset 1
 	f_mean = 4
 	f_std = 3.5
-	g_lam = 40 
+	g_lam = 50 
 	discount = .5
 	num_auctions = 40
 	num_datasets = 15
+	num_trials = 20
+	max_iterations = 1000
 	auction = Auction_Simulator(f_mean, f_std, g_lam)
 
 	data_1 = [[auction.simulate_initial_auction() for k in range(num_auctions)] for i in range(num_datasets)]
@@ -19,10 +21,10 @@ def main():
 
 	auction_set, all_bids, num_bidders = data_1[0][0]
 
-	em1 = EM(auction_set, discount)
-	est_f_mean, est_f_std, est_g_lam, iterations = em1.learn()
+	em1 = EM(auction_set, discount, num_trials, max_iterations)
+	est_f_mean, est_f_std, est_g_lam = em1.learn()
 
-	print "RESULT: ", iterations, est_f_mean, est_f_std, est_g_lam
+	print "RESULT AVG: ", est_f_mean, est_f_std, est_g_lam
 
 	print "ACTUAL: ", average(all_bids), std(all_bids), num_bidders
 
